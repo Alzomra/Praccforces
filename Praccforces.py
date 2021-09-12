e59b64a3f39b495d854b11bceb2186d5a851261e
Oussema <65419434+Alzomra@users.noreply.github.com>
2021-09-12 23:28:44 +0100

    Update Praccforces.py

--- Praccforces.py
+++ Praccforces.py
@@ -9,7 +9,7 @@ from db import pracc_users
 intents = discord.Intents.default()
 intents.members = True 
 intents.presences = True
-# Working with envs HMMMMMMDsdafdadaddadadadadaadda
+# Working with envs HMMMMMMDsdafdadaddadadadadaaddadad
 
 
 
