7e8f8f083d00f1e41a0a09c27168f8f4ca5178fd
Oussema <65419434+Alzomra@users.noreply.github.com>
2021-09-13 16:54:42 +0100

    Update Praccforces.py

--- Praccforces.py
+++ Praccforces.py
@@ -9,7 +9,7 @@ from db import pracc_users
 intents = discord.Intents.default()
 intents.members = True 
 intents.presences = True
-# Working with envs HMMMMMMDsdafdadaddadadadadaaddadadFAKdsdadadadaddadad WORKINGddd
+# Working with envs HMMMMMMDsdafdadaddadadadadaaddadadFAKdsdadadadaddadad WORKINGddddada
 
 
 client = commands.Bot(command_prefix = "!" , intents = intents , case_insensitive = True)
