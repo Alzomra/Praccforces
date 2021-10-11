17c37cb8dc8ec92397d16d2b076d3f9a9eaaf0c5
Oussema <65419434+Alzomra@users.noreply.github.com>
2021-10-11 22:41:04 +0100

    Optimized problemset

--- cogs/problemset.py
+++ cogs/problemset.py
@@ -1,4 +1,5 @@
 import discord
+from discord import embeds
 from discord.embeds import Embed
 from discord.ext import commands
 import random
@@ -6,7 +7,7 @@ import os
 from discord.ext.commands.cog import Cog
 from discord.ext.commands.errors import CommandError
 import requests
-from db import pracc_users , pracc_servers
+from db import pracc_users , pracc_servers , pracc_problems
 from checks import check_user , check_guild
 
 
@@ -20,11 +21,9 @@ def cleaned_args(args):
             try: 
                 arg = int(arg)
             except:
-                print("Not a number")
-                new_args.append(arg)
+                new_args.append(arg.lower())
             else : 
                 if not diffb : 
-                    print("found IT" , arg)
                     diff = arg
                     diffb = True
         return new_args , diff
@@ -46,47 +45,39 @@ class Problemset(commands.Cog):
         await check_guild(ctx)
         await check_user(ctx.author)
         args  , diff = cleaned_args(args)
-        tag = ' '.join(args)
-        if tag.lower() == "tags" : 
+        tags = args
+        if not args : 
+            tags = ''
+
+        if tags[0].lower() == "tags" : 
             em = discord.Embed(color = 0xFFFFFF)
             em.title = "All Tags"
             em.description = " * ".join(self.tags)
             await ctx.send(embed = em)
             return
-        
-        if tag not in self.tags : 
-            raise commands.CommandError(message="Unvalid tag")
-            return 
+
+        for tag in tags :
+            print(tag , tag in self.tags)
+            if tag not in self.tags : 
+                raise commands.CommandError(message="Unvalid Tag")
+                return 
           
         colors = [0xF0EF17 , 0x11A6F9 , 0xF80A0A]
-        
-        problems = requests.get(f"https://codeforces.com/api/problemset.problems?tags={tag.replace(' ','')}")
-        if diff : 
-            probs = []
-            ps= sorted(problems.json()["result"]["problems"] , key = lambda x : x["contestId"])
-            for p in ps : 
-                try : 
-                    if abs(p["rating"] - diff) < 100 : 
-                        probs.append(p)
-                except :
-                    pass
-            if probs : 
-                prob = random.choice(probs)
+        tags.sort()
 
-        if not diff or probs == [] :
-            prob = random.choice(problems.json()["result"]["problems"])
-        try  :
-            rating = prob['rating']
-        except :
-            try : 
-                rating = prob['points']
-            except : 
-                rating = "NULL"
+        problems = list(pracc_problems.find({"tags" : tags , "rating" : diff or {'$gt' : diff} }))
 
+        if not problems:
+            problems = list(pracc_problems.find({"tags" : {"$all" : tags} , "rating" : diff or {'$gt' : diff} }))
+            if not problems : 
+                raise commands.CommandError(message="No Problem Found")
+                return
+
+        prob = random.choice(problems)
         em = discord.Embed(color = random.choice(colors))
         em.title = prob["name"]
-        em.add_field(name = "Link :" , value = f"https://codeforces.com/problemset/problem/{prob['contestId']}/{prob['index']}", inline=False)
-        em.add_field(name = "Rating" , value = rating, inline=False)
+        em.add_field(name = "Link :" , value = prob["link"], inline=False)
+        em.add_field(name = "Rating" , value = prob["rating"], inline=False)
         if tag == "" :
             em.add_field(name = "Tags" , value="||" + "|| ||".join(prob["tags"]) + "||", inline=False)
         else : 
@@ -96,17 +87,21 @@ class Problemset(commands.Cog):
     
     @problem.error
     async def problem_error(self,ctx,error):
-        if error == "Unvalid Tag" : 
-            await ctx.send("Unvalid TAG , Use '!problem tags' to get the full list of Tags.")   
+        em = discord.Embed(color = 0XFF0000)
+        em.title = "Problem Error !"
+        if str(error) == "Unvalid Tag" : 
+            em.description = "Unvalid TAG , Use '!problem tags' to get the full list of Tags."
+        if str(error) == "No Problem Found" : 
+            em.description = "No Problem Found with provided arguments , Use '!problem tags' to get the full list of Tags.\n try changing the rating or vary the tags"
+        await ctx.send(embed = em) 
+    
 
     @commands.command()
     async def solution(self,ctx,arg):
         if not arg : 
-            print('no args')
-        print(arg)
+            return 
         arg = arg.replace('.','').replace('/','').replace('\\','').replace('"','').replace("'",'').replace('*','').replace('$','')
         arg = arg.upper()
-        print(arg)
         if (not os.path.exists(f"D:\ACM\Codeforces Problemset\ProblemSet\{arg}.cpp")) : 
             return
         with open(f"D:\ACM\Codeforces Problemset\ProblemSet\{arg}.cpp") as f : 
