14a65b13023bc582f425ade85639afe2c1b7a977
Oussema <65419434+Alzomra@users.noreply.github.com>
2021-10-22 22:00:38 +0100

    shard count

--- cogs/server_count.py
+++ cogs/server_count.py
@@ -9,7 +9,7 @@ class TopGG(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc5NDkwMTE1Njg5MDY3MzE2MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEwODAwNDAzfQ.-ug8IuwFzAPgAoOfsHHRi4jcEYFNN-uEFtEiU3bevhY' # set this to your DBL token
-        self.dblpy = topgg.DBLClient(self.client, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
+        self.dblpy = topgg.DBLClient(self.client, self.token, autopost=True , post_shard_count=True) # Autopost will post your guild count every 30 minutes
 
     async def on_guild_post(self):
         print("Server count posted successfully")
